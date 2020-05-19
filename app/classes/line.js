c5b6d0d06345105ef2c3ce6e5b7321c22729fcfd
Y.Hasegawa <hasegawa@zaf.jp>
2020-05-19 18:35:57 +0900

    copy function richMenuId

--- app/classes/line.js
+++ app/classes/line.js
@@ -11,50 +11,5 @@ module.exports = class Line {
             channelSecret: config[channel]['secret']
         });
     }
-
-    /*
-    richMenus() {
-        return this.client.getRichMenuList();
-    }
-
-    richMenu(menuId) {
-        return this.client.getRichMenu(menuId);
-    }
-
-    richMenuImage(menuId) {
-        return this.client.downloadRichMenuImage(menuId);
-    }
-
-    deleteRichMenu(menuId) {
-        return this.client.deleteRichMenu(menuId);
-    }
-
-    async createRichMenu(menu, buffer) {
-        for (const area of menu['areas']) {
-            if (area['action']['type'] === 'datetimepicker') {
-                delete area['action']['initial'];
-                delete area['action']['max'];
-                delete area['action']['min'];
-            }
-        }
-
-        const created = await this.client.createRichMenu(menu).catch((err) => {
-            throw err;
-        });
-
-        return this.client.uploadRichMenuImage(
-            created['richMenuId'],
-            buffer
-        );
-    }
-
-    setDefaultRichMenu(menuId) {
-        return this.client.setDefaultRichMenu(menuId);
-    }
-
-    defaultRichMenu() {
-        return this.client.getDefaultRichMenu();
-    }
-    */
 }
 
--- src/js/init.js
+++ src/js/init.js
@@ -3,9 +3,17 @@ const M = require('materialize-css');
 
 (() => {
     M.AutoInit();
+
+    $('.copy-clip').on('click', function() {
+        const text = $(this).text();
+        $('#copy-boad').text(text).show().select();
+        document.execCommand('copy');
+        $('#copy-boad').hide();
+        M.toast({html: 'Copied "' + text + '"'});
+    });
+
     $('#channel-selector').on('change', function() {
         $.post('/channel', {channel: $(this).val()}).done((response) => {
-            console.log(response);
             location.reload();
         }).fail((response) => {
             console.log(response);
