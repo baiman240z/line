280354dd32c990493f27f999f9d6310cf1169a2c
Y.Hasegawa <hasegawa@zaf.jp>
2020-05-19 18:11:00 +0900

    reload on change channel

--- src/js/init.js
+++ src/js/init.js
@@ -6,6 +6,7 @@ const M = require('materialize-css');
     $('#channel-selector').on('change', function() {
         $.post('/channel', {channel: $(this).val()}).done((response) => {
             console.log(response);
+            location.reload();
         }).fail((response) => {
             console.log(response);
         });
